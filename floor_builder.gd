c628aa0d6cdf12782c075f66f2e7e34d2126da04
Kris Akers <gruntworkgames>
2024-01-07 02:13:22 -0600

    extracted a floor builder from the scene

---
+++ floor_builder.gd
@@ -0,0 +1,53 @@
+class_name FloorBuilder extends Node3D
+
+var grassblock = preload("res://grass_cube.tscn")
+const blocksize = 2
+var blocks = []
+const numSpaces = 40
+var floor
+var floorTiles = []
+var wallTiles = [] 
+var decorator = FloorDecorator.new()
+var r = RandomNumberGenerator.new()
+var scene
+
+func createmap(s):
+	scene = s
+	_clearblocks()
+	var floorfactory = FloorFactory.new()
+	var floorsize = Vector3(8,0,12)
+	floor = floorfactory.generate(floorsize, numSpaces)
+	_buildFromFloor(floor, floorsize)
+
+func _buildFromFloor(floor, floorsize):
+	for x in floorsize.x:
+		for z in floorsize.z:
+			var isWall = floor.walls[x][z]
+			if isWall:
+				var block = _create(Vector3(x * blocksize, 0, z * blocksize))
+				blocks.append(block)
+				wallTiles.append(Vector3(x,0,z))
+			else:
+				var tile = _createSteppingStone(Vector3(x * blocksize, 0, z * blocksize))
+				blocks.append(tile)
+				floorTiles.append(Vector3(x,0,z))
+
+func _clearblocks():
+	for block in blocks:
+		scene.remove_child(block)
+	blocks.clear()	
+	wallTiles.clear()
+	floorTiles.clear()
+
+func _create(pos):
+	var instance = grassblock.instantiate()
+	instance.position = pos
+	scene.add_child(instance)
+	return instance
+
+func _createSteppingStone(pos):
+	var instance = decorator.randomFloorTileModel()
+	instance.position = pos;
+	instance.rotation.y = r.randf()
+	scene.add_child(instance)
+	return instance
--- scene.gd
+++ scene.gd
@@ -1,62 +1,11 @@
 extends Node3D
 
-var grassblock = preload("res://grass_cube.tscn")
-const blocksize = 2
-var blocks = []
-const numSpaces = 40
-var floor
-var floorTiles = []
-var wallTiles = []
-var decorator = FloorDecorator.new()
-var r = RandomNumberGenerator.new()
+var floor_builder = FloorBuilder.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_createmap()
-
-func _buildFromFloor(floor, floorsize):
-	for x in floorsize.x:
-		for z in floorsize.z:
-			var isWall = floor.walls[x][z]
-			if isWall:
-				var block = _create(Vector3(x * blocksize, 0, z * blocksize))
-				blocks.append(block)
-				wallTiles.append(Vector3(x,0,z))
-			else:
-				var tile = _createSteppingStone(Vector3(x * blocksize, 0, z * blocksize))
-				blocks.append(tile)
-				floorTiles.append(Vector3(x,0,z))
+	floor_builder.createmap(self)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		_clearblocks()
-		_createmap()				
-
-func _createmap():
-	var floorfactory = FloorFactory.new()
-	var floorsize = Vector3(8,0,12)
-	floor = floorfactory.generate(floorsize, numSpaces)
-	_buildFromFloor(floor, floorsize)
-
-func _clearblocks():
-	for block in blocks:
-		remove_child(block)
-	blocks.clear()	
-	wallTiles.clear()
-	floorTiles.clear()
-
-func _process(delta):
-	pass
-
-func _create(pos):
-	var instance = grassblock.instantiate()
-	instance.position = pos
-	add_child(instance)
-	return instance
-
-func _createSteppingStone(pos):
-	var instance = decorator.randomFloorTileModel()
-	instance.position = pos;
-	instance.rotation.y = r.randf()
-	add_child(instance)
-	return instance
+		floor_builder.createmap(self)				
